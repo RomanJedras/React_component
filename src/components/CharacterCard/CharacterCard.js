5158b7ae9fb98d90c44751075b82a8eddb5a911d
Roman Jędras <romanjedras@o2.pl>
2019-07-03 16:29:32 +0200

    Understand Pattern Component React

---
+++ src/components/CharacterCard/CharacterCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProfileRow from './ProfileRow/ProfileRow';
+
+
+
+const UNKNOWN_AVATAR_URL = `${
+  process.env.PUBLIC_URL
+  }/images/unknown-avatar.png`;
+
+const CharacterCard = ({ name, image, species, gender }) => {
+  return (
+    <div className="id-card-wrapper" key={name}>
+      <div className="id-card">
+        <div className="character-card">
+          <ProfileRow name={name} image={image} gender={gender} species={species}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+CharacterCard.propTypes = {
+  gender: PropTypes.string,
+  image: PropTypes.string,
+  name: PropTypes.string,
+  species: PropTypes.string,
+};
+
+CharacterCard.defaultProps = {
+  name: 'John Doe',
+  image: UNKNOWN_AVATAR_URL,
+  gender: 'n/a',
+  species: 'human',
+};
+
+
+export default  CharacterCard;
---
+++ src/containers/RickAndMortyCharactersCards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import CharactersCards from '../components/CharactersCards';
+
+
+const API_URL = 'https://rickandmortyapi.com/api/character/';
+
+
+class RickAndMortyCharactersCards extends React.Component {
+
+   constructor (props) {
+     super(props)
+     
+     this.state = {
+       characters: []
+     }
+   }
+  
+  componentDidMount() {
+    axios.get(API_URL).then(({ data: { results } }) => {
+      this.setState({ characters: results });
+    });
+  }
+  
+ 
+  render() {
+    return <CharactersCards characters={this.state.characters} />;
+   }
+}
+
+
+export default RickAndMortyCharactersCards;
